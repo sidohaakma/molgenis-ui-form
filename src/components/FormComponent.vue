96434db950f5aabd8ea5e0a0a9402d589e08133a
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2017-12-21 10:39:44 +0100

    Setup for textField field type.

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,21 +1,40 @@
 <template>
-  <div>
-    <form>
-      <div class="form-group">
-        <label for="textInput">Fill in something</label>
-        <input v-model="msg" class="form-control" type="text" id="textInput"/>
-      </div>
-    </form>
-  </div>
+  <vue-form :id="id" :state="state">
+    <fieldset v-for="field in schema.fields">
+      <text-field-component v-model="data[field.id]" :field="field" :state="state"></text-field-component>
+    </fieldset>
+  </vue-form>
 </template>
 
 <script>
-export default {
-  name: 'FormComponent',
-  data () {
-    return {
-      msg: 'Hello'
+  import VueForm from 'vue-form'
+  import TextFieldComponent from './field-types/TextFieldComponent'
+
+  export default {
+    name: 'FormComponent',
+    mixins: [VueForm],
+    components: {
+      TextFieldComponent
+    },
+    props: {
+      id: {
+        type: String,
+        required: true
+      },
+      schema: {
+        type: Object,
+        required: true
+      },
+      data: {
+        type: Object,
+        required: false,
+        default: () => ({})
+      }
+    },
+    data () {
+      return {
+        state: {}
+      }
     }
   }
-}
 </script>
---
+++ src/components/field-types/TextFieldComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <validate :state="state" :custom="{'validators': validators}">
+    <div class="form-group">
+      <label :for="field.id">{{ field.label }}</label>
+
+      <input :id="field.id"
+             v-model="localValue"
+             :type="field.type"
+             :name="field.id"
+             class="form-control form-control-lg"
+             :class="{ 'is-invalid' : state && (state.$touched || state.$submitted) && state.$invalid}"
+             :aria-describedby="field.id + '-description'"
+             :required="field.required"
+             :disabled="field.disabled">
+
+      <small :id="field.id + '-description'" class="form-text text-muted">
+        {{ field.description }}
+      </small>
+
+      <field-messages :name="field.id" show="$touched || $submitted" class="form-control-feedback">
+        <div slot="required">This field is required</div>
+        <div slot="validators">{{ validationMessage }}</div>
+      </field-messages>
+    </div>
+  </validate>
+</template>
+
+<script>
+  import VueForm from 'vue-form'
+
+  export default {
+    name: 'text-field-component',
+    props: ['value', 'field', 'state'],
+    mixins: [VueForm],
+    data () {
+      return {
+        // Store a local value to prevent changing the parent state
+        localValue: this.value,
+        validationMessage: ''
+      }
+    },
+    methods: {
+      validators () {
+        let valid = true
+        this.field.validators.forEach(validator => {
+          const result = validator(this.localValue)
+          this.validationMessage = result.message
+          valid = result.valid
+        })
+        return valid
+      }
+    },
+    watch: {
+      localValue (value) {
+        // Emit value changes to the parent (form)
+        this.$emit('input', value)
+      }
+    }
+  }
+</script>
