c28d7d249fff1552e56071d3e19d1b830ef899c2
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2017-12-27 16:36:15 +0100

    Setup flow type checking
    - Setup flow
    - Add type for mapping schema ( WIP needs more detail)
    - Add typing for field types + extend test

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -7,6 +7,7 @@ const baseWebpackConfig = require('./webpack.base.conf')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+const FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -50,6 +51,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true
     }),
+    new FlowBabelWebpackPlugin(),
   ]
 })
 
