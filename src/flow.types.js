a827b5a66f6c5028dd418ec8cd3e419bc06c243c
Mark-de-Haan <markdehaan90@gmail.com>
2018-01-03 14:36:18 +0100

    Update checkbox flow type

--- src/flow.types.js
+++ src/flow.types.js
@@ -3,7 +3,7 @@
 export type EntityFieldType = 'BOOL' | 'CATEGORICAL' | 'ENUM' | 'XREF' |'MREF' |'ONETOMANY' |'INT' | 'DECIMAL' | 'LONG' |
   'TEXT' | 'SCRIPT'| 'HTML' | 'DATE'| 'DATE_TIME'| 'CATEGORICAL_MREF'| 'STRING'| 'HYPERLINK'| 'EMAIL' | 'FILE'
 
-export type HtmlFieldType = 'radio' | 'select' | 'number' | 'text-area' | 'date' | 'date-time' | 'checkboxes' |
+export type HtmlFieldType = 'radio' | 'select' | 'number' | 'text-area' | 'date' | 'date-time' | 'checkbox' |
   'text' | 'url' | 'email' | 'file'
 
 export type FormField = {
