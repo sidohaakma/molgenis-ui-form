6fae861848e18e9356a08321e1b49f2cb96167b6
sido <sido@haakma.org>
2018-01-10 16:50:58 +0100

    [FIX] added unit test for toggle

--- test/unit/specs/FormComponent.spec.js
+++ test/unit/specs/FormComponent.spec.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueForm from 'vue-form'
 import FormComponent from '@/components/FormComponent'
+import { shallow } from 'vue-test-utils'
 
 describe('FormComponent unit tests', () => {
   it('should load the component with "FormComponent" as a name', () => {
@@ -26,5 +27,49 @@ describe('FormComponent unit tests', () => {
     const propsData = {id: 'test-form', schema: {fields: []}}
     const vm = new Constructor({propsData: propsData, mixins: [VueForm]}).$mount()
     expect(vm.$el.id).to.equal('test-form')
+  })
+})
+
+describe('FormComponents shallow tests', () => {
+  const field = {
+    id: 'string',
+    type: 'text',
+    validate: (data) => data['string'] === 'data',
+    visible: () => true,
+    required: () => true
+  }
+
+  const state = {
+    'string': {
+      $touched: false,
+      $submitted: false,
+      $invalid: false,
+      _addControl: () => {}
+    }
+  }
+
+  const propsData = {
+    id: 'test',
+    data: {'string': 'data'},
+    schema: {
+      fields: [ field ]
+    },
+    state: state,
+    hooks: {
+      onSubmit: () => true,
+      onCancel: () => true
+    }
+  }
+
+  const wrapper = shallow(FormComponent, {
+    propsData: propsData
+  })
+
+  describe('toggle show optional fields', () => {
+    it('should toggle to false', () => {
+      wrapper.setData({state: {}, showOptionalFields: true})
+      wrapper.vm.toggleOptionalFields()
+      expect(wrapper.vm.showOptionalFields).to.equal(false)
+    })
   })
 })
