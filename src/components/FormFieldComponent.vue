981a96d2a94fe03b7df0ff64f51dffebd19138a8
Mark-de-Haan <markdehaan90@gmail.com>
2018-01-17 17:17:55 +0100

    Added FileFieldComponent

--- src/components/FormFieldComponent.vue
+++ src/components/FormFieldComponent.vue
@@ -13,6 +13,18 @@
       </checkbox-field-component>
     </template>
 
+    <!-- Render file field -->
+    <template v-else-if="field.type === 'file'">
+      <file-field-component
+        v-model="formData[field.id]"
+        :field="field"
+        :state="state[field.id]"
+        :isValid="isValid"
+        :isRequired="isRequired"
+        @dataChange="onDataChange">
+      </file-field-component>
+    </template>
+
     <!-- Render field groups + child fields, nesting subsequent groups with padding -->
     <template v-else-if="field.type === 'field-group'">
       <legend>{{ field.label }}</legend>
@@ -117,6 +129,7 @@
 <script>
   import CheckboxFieldComponent from './field-types/CheckboxFieldComponent'
   import DateFieldComponent from './field-types/DateFieldComponent'
+  import FileFieldComponent from './field-types/FileFieldComponent'
   import MultiSelectFieldComponent from './field-types/MultiSelectFieldComponent'
   import RadioFieldComponent from './field-types/RadioFieldComponent'
   import SingleSelectFieldComponent from './field-types/SingleSelectFieldComponent'
@@ -169,6 +182,7 @@
     components: {
       CheckboxFieldComponent,
       DateFieldComponent,
+      FileFieldComponent,
       MultiSelectFieldComponent,
       RadioFieldComponent,
       SingleSelectFieldComponent,
