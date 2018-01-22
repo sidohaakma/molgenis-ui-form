6a8f12f767739967d83340a74b6e1216a665fd04
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-01-22 14:56:42 +0100

    Style v-select to look like bootstrap field

--- src/FormDemo.vue
+++ src/FormDemo.vue
@@ -32,6 +32,31 @@
   </div>
 </template>
 
+<style>
+  /*  Styling to have v-select look like bootstrap field */
+  .v-select {
+    padding: 0 0 0 10px;
+  }
+
+  .v-select.disabled {
+    background-color: #f8f8f8;
+  }
+
+
+  .v-select > .dropdown-toggle {
+    border: none;
+  }
+
+  /* fix to hide input[type=search] as webkit forces browser style*/
+  .v-select .dropdown-toggle input[type=search] {
+    width: 0;
+    padding: 0;
+    margin: 0;
+    -webkit-appearance: textfield;
+    z-index: -1;
+  }
+</style>
+
 <script>
   import { EntityToStateMapper, FormComponent } from './molgenisUiForm'
   import EntityTypeV2Response from './formDemoMockResponse'
