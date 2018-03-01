eeb1af30081bdda1af9c7b939bbe7405fd4d8515
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-03-01 12:32:40 +0100

    Localize validation messages via molgenis/i18n if available else use defaults

--- src/components/FormFieldMessages.vue
+++ src/components/FormFieldMessages.vue
@@ -1,16 +1,24 @@
 <template>
   <field-messages :name="fieldId" :state="fieldState" show="$touched || $submitted" class="form-control-feedback">
-    <div class="invalid-message" slot="required">This field is required</div>
-    <div class="invalid-message" slot="validate">Validation failed</div>
-    <div v-if="type === 'number'" class="invalid-message" slot="number">Not a valid number</div>
-    <div v-if="type === 'url'" class="invalid-message" slot="url">Not a valid URL</div>
-    <div v-if="type === 'email'" class="invalid-message" slot="email">Not a valid email</div>
+    <div class="invalid-message" slot="required">{{ requiredFieldMsg }}</div>
+    <div class="invalid-message" slot="validate">{{ validationFailedMsg }}</div>
+    <div v-if="type === 'number'" class="invalid-message" slot="number">{{ notAValidNumberMsg }}</div>
+    <div v-if="type === 'url'" class="invalid-message" slot="url">{{ notAValidUrlMsg }}</div>
+    <div v-if="type === 'email'" class="invalid-message" slot="email">{{ notAValidEmailMsg }}</div>
   </field-messages>
 </template>
 
 <script>
   import VueForm from 'vue-form'
   import { HtmlFieldType } from '../flow.types'
+
+  const defaultMessages = {
+    form_required_field: 'This field is required',
+    form_validation_failed: 'Validation failed',
+    form_not_a_valid_number: 'Not a valid number',
+    form_not_a_valid_url: 'Not a valid URL',
+    form_not_a_valid_email: 'Not a valid email'
+  }
 
   export default {
     name: 'FormFieldMessages',
@@ -27,6 +35,28 @@
       fieldState: {
         type: Object
       }
+    },
+    data () {
+      return {
+        localizedMessages: {}
+      }
+    },
+    methods: {
+      getLocalizedMessage (msgKey) {
+        return this.localizedMessages(msgKey)
+      },
+      getDefaultMessage (msgKey) {
+        return defaultMessages[msgKey]
+      }
+    },
+    created () {
+      this.localizedMessages = this.$t || this.getDefaultMessage
+
+      this.requiredFieldMsg = this.getLocalizedMessage('form_required_field')
+      this.validationFailedMsg = this.getLocalizedMessage('form_validation_failed')
+      this.notAValidNumberMsg = this.getLocalizedMessage('form_not_a_valid_number')
+      this.notAValidUrlMsg = this.getLocalizedMessage('form_not_a_valid_url')
+      this.notAValidEmailMsg = this.getLocalizedMessage('form_not_a_valid_email')
     }
   }
 </script>
