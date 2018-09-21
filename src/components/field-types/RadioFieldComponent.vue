605b87ed2302f196012a07bb2e63862468c3ee79
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-09-21 10:48:58 +0200

    Trigger field validation when field becomes dirty
    - only validate after debounced value update

--- src/components/field-types/RadioFieldComponent.vue
+++ src/components/field-types/RadioFieldComponent.vue
@@ -12,7 +12,7 @@
           type="radio"
           :name="field.id"
           class="form-check-input"
-          :class="{ 'is-invalid' : fieldState && (fieldState.$touched || fieldState.$submitted) && fieldState.$invalid}"
+          :class="{ 'is-invalid' : fieldState && (fieldState.$touched || fieldState.$submitted || fieldState.$dirty) && fieldState.$invalid}"
           :required="isRequired"
           :disabled="field.disabled"
           :bool="localValue === true || localValue === false">
