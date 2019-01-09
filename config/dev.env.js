dd7162f2c3c7e1aaf41ec0b0d5e68bed1d0f6bb2
sidohaakma <sido@haakma.org>
2019-01-09 12:29:53 +0100

    fix(dev.env.js): added Jenkins hostname configuration to fix safari bug

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  // In CI mode, Safari cannot contact "localhost", so as a workaround, run the dev server using the jenkins agent pod dns instead.
+  host: process.env.JENKINS_AGENT_NAME || 'localhost'
 })
